5cc6cd1d1df71bd12acc0b692430d31f08a7f947
Kasvitha <kasvitha2001@gmail.com>
2025-03-20 22:06:13 -0400

    kasvi

--- static/config.js
+++ static/config.js
@@ -14,6 +14,6 @@
  * @type {{apiKey: string, authDomain: string}}
  */
 const config = {
-  apiKey: 'AIzaSyDhwsk8AklD8Ipj26fVzYTMgrWvE8x1rgc',
-  authDomain: 'cc-spring2025.firebaseapp.com',
+  apiKey: 'AIzaSyB4qJKP5Ra0IbGc3WFhnqaVIPGVKd7ymtw',
+  authDomain: 'cloud-ru.firebaseapp.com',
 };
